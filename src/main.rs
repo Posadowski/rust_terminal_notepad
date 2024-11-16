14a7d862715779efa93011e1c01e406db5ac7b2b
Posadowski <mateuszposa@gmail.com>
2024-11-16 16:08:57 +0100

    feat: use crossterm

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,15 @@ use std::fs::File;
 use std::io::Write;
 use std::sync::{Arc, Mutex};
 
+use crossterm::{
+    cursor,
+    event::{self, Event, KeyCode,KeyModifiers},
+    execute,
+    style::{Print},
+    terminal::{self},
+};
+use crossterm::event::KeyEventKind;
+
 fn main() -> std::io::Result<()>{
     let file_name;
     if std::env::args().count() < 2 {
@@ -9,28 +18,93 @@ fn main() -> std::io::Result<()>{
     } else {
         file_name = std::env::args().collect::<Vec<_>>()[1].clone().to_string();
     }
+
+    // terminal init
+    let mut stdout = std::io::stdout();
+    terminal::enable_raw_mode()?;
+    execute!(stdout, terminal::EnterAlternateScreen, cursor::Hide)?;
+
     // Variable to store all input from user
-    let inserted_text = Arc::new(Mutex::new(String::new()));
+    let inserted_text = Arc::new(Mutex::new(String::new())); // user text buffer
+    let mut cursor_position = (0,0);
 
-    // ctrl+c handler
-    {
-        let file_name = file_name.clone();
-        let inserted_text = Arc::clone(&inserted_text);
-
-        ctrlc::set_handler(move || {
-            let text = inserted_text.lock().unwrap();
-            let mut file = File::create(&file_name).expect("Cannot create file");
-            file.write_all(text.as_bytes()).expect("Failed to write to file");
-            std::process::exit(0);
-            }).expect("Error setting Ctrl-C handler");
-    }
     loop {
-        let mut ret = String::new();
-        std::io::stdin().read_line(&mut ret).expect("Failed to read from stdin");
-        // Adding input to a variable stored in Arc<Mutex<String>>
-        {
-            let mut text = inserted_text.lock().unwrap();
-            text.push_str(&ret);
+
+        // clear screen and show buffer
+        { // put it in a separate scope to free up the mutex on the inserted_text variable
+            let text = inserted_text.lock().unwrap();
+            execute!(
+                stdout,
+                terminal::Clear(terminal::ClearType::All),
+                cursor::MoveTo(0, 0),
+                Print(&*text),
+                cursor::MoveTo(cursor_position.0, cursor_position.1),
+            )?;
+        }
+        if let Event::Key(key_event) = event::read()? {
+            if key_event.kind != KeyEventKind::Release { // skip releasing the button
+                match key_event {
+                    event::KeyEvent {
+                        code: KeyCode::Char(c),
+                        modifiers: KeyModifiers::NONE,
+                        ..
+                    } => {
+                        println!("Get char");
+                        let mut text = inserted_text.lock().unwrap();
+                        text.push(c);
+                        cursor_position.0 += 1;
+                    }
+
+                    event::KeyEvent {
+                        code: KeyCode::Backspace,
+                        ..
+                    } => {
+                        println!("Get backspace");
+                        let mut text = inserted_text.lock().unwrap();
+                        if !text.is_empty() {
+                            text.pop();
+                            if cursor_position.0 > 0 {
+                                cursor_position.0 -= 1;
+                            }
+                        }
+                    }
+
+                    event::KeyEvent {
+                        code: KeyCode::Enter,
+                        ..
+                    } => {
+                        println!("Get eneter");
+                        let mut text = inserted_text.lock().unwrap();
+                        text.push('\n');
+                        cursor_position.0 = 0;
+                        cursor_position.1 += 1;
+                    }
+
+                    event::KeyEvent {
+                        code: KeyCode::Esc,
+                        ..
+                    } => {
+                        println!("Get esc");
+                        break;
+                    }
+                    event::KeyEvent {
+                        code: KeyCode::Char('c'),
+                        modifiers: KeyModifiers::CONTROL,
+                        ..
+                    } => {
+                        println!("Get ctrl+c");
+                        let text = inserted_text.lock().unwrap();
+                        let mut filr = File::create(&file_name)?;
+                        filr.write_all(text.as_bytes())?;
+                        break;
+                    }
+
+                    _ => {}
+                }
+            }
         }
     }
+    execute!(stdout, terminal::LeaveAlternateScreen, cursor::Show)?;
+    terminal::disable_raw_mode()?;
+    Ok(())
 }
